7474ef5bd63022e1c55d845e9f19d75e885296e9
lianaschapiro <liana.schapiro@gmail.com>
2019-01-24 23:07:33 -0500

    show new post after save

--- js/main.js
+++ js/main.js
@@ -103,3 +103,44 @@ function newPost() {
     `<button class="cta cta--all-posts">&#8592; See All Posts</button>`
   document.getElementById('footer').innerHTML = footer;
 }
+
+// Save new post with POST request
+function newPostSave() {
+  let title = document.getElementById('new-post__title').value;
+  let body = document.getElementById('new-post__body').value;
+  let userId = document.getElementById('new-post__user-id').value;
+  console.log(title)
+  console.log(body)
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: title,
+      body: body,
+      userId: userId
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+
+  .then(response => response.json())
+  .then(json => newPostShow(json))
+  .catch(error => console.error(error))
+}
+
+// Display new post after saving
+function newPostShow(postData) {
+  const { id, title, body, userId } = postData
+  let result =
+    `<h3 class="alert">your post has been saved</h3>
+    <div class="post-view__post">
+      <h1 class="post-view__title">${title}</h1>
+      <p class="post-view__body">${body}</p>
+      <p class="post-view__user-id">By: User #${userId}</p>
+    </div>`
+  document.getElementById('result').innerHTML = result;
+  let footer =
+    `<button class="cta cta--all-posts">&#8592; See All Posts</button>
+    <button class="cta cta--new-post">Write Another Post</button>`
+  document.getElementById('footer').innerHTML = footer;
+}
