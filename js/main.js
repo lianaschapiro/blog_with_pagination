75a74900fb73891225066edb6abc584fbd47cffa
lianaschapiro <liana.schapiro@gmail.com>
2019-01-24 23:01:31 -0500

    show individual posts

--- js/main.js
+++ js/main.js
@@ -43,3 +43,46 @@ function fetchPosts(url) {
     .catch(error => console.error(error))
 }
 fetchPosts('https://jsonplaceholder.typicode.com/posts?_page=1');
+
+// Show individual post
+function fetchOnePost(id) {
+  fetch('https://jsonplaceholder.typicode.com/posts/'+id+'')
+    .then(response => {return response.json() })
+    .then((json) => {
+      const { id, title, body, userId } = json
+      let result =
+        `<div class="post-view__post">
+          <img class="post-view__img" src="http://lorempixel.com/640/360">
+          <h2 class="post-view__title">${title}</h2>
+          <p class="post-view__body">${body}</p>
+        </div>`;
+      document.getElementById('result').innerHTML = result;
+      let footer =
+        `<button class="cta cta--all-posts">&#8592; See All Posts</button>
+        <button class="cta cta--new-post">New Post</button>`
+      document.getElementById('footer').innerHTML = footer;
+    })
+    .catch(error => console.error(error))
+}
+
+// 'Show All Posts' button
+$(document).on('click', '.cta--all-posts', function(){
+  fetchPosts('https://jsonplaceholder.typicode.com/posts?_page=1');
+})
+
+// 'New Post' button
+$(document).on('click', '.cta--new-post', function(){
+  newPost();
+})
+
+// Header click event
+$(document).on('click', '.header', function(){
+  fetchPosts('https://jsonplaceholder.typicode.com/posts?_page=1');
+})
+
+// Show individual post from post list
+$(document).on('click', '.post-list__post', function(){
+  var id = $(this).data('id');
+  fetchOnePost(id);
+})
+
